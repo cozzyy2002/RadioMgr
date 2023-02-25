8251ca02ee50aa95de01533ab7725a034e2426ce
Koji Yano <cozzyy2002@gmail.com>
2023-02-26 00:09:30 +0900

    Minor change.

--- RadioMgrConsole/BluetoothRadio.cpp
+++ RadioMgrConsole/BluetoothRadio.cpp
@@ -8,11 +8,11 @@ static void HandleDeleteFunc(HANDLE h) { WIN32_EXPECT(CloseHandle(h)); }
 HRESULT EnumBluetoothRadios()
 {
 	BLUETOOTH_FIND_RADIO_PARAMS params{sizeof(params)};
-	HANDLE _hRadio;
-	auto hFind = BluetoothFindFirstRadio(&params, &_hRadio);
+	HANDLE hRadio;
+	auto hFind = BluetoothFindFirstRadio(&params, &hRadio);
 	WIN32_ASSERT(hFind);
 	do {
-		SafeHandle<HANDLE, HandleDeleteFunc> hRadio(_hRadio);
+		SafeHandle<HANDLE, HandleDeleteFunc> _h(hRadio);
 
 		BLUETOOTH_RADIO_INFO info{sizeof(info)};
 		HR_ASSERT_OK(HRESULT_FROM_WIN32(BluetoothGetRadioInfo(hRadio, &info)));
@@ -22,7 +22,7 @@ HRESULT EnumBluetoothRadios()
 			L"  Class of Device = %s\n",
 			A2ARG(info, address), info.szName, info.manufacturer, info.lmpSubversion,
 			ClassOfDevice(info.ulClassofDevice).c_str());
-	} while(BluetoothFindNextRadio(hFind, &_hRadio));
+	} while(BluetoothFindNextRadio(hFind, &hRadio));
 	auto hr = WIN32_EXPECT(GetLastError() == ERROR_NO_MORE_ITEMS);
 	WIN32_ASSERT(BluetoothFindRadioClose(hFind));
 

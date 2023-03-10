d8062d5d6dca6eb2b89bceb3997fe3a062783755
Koji Yano <cozzyy2002@gmail.com>
2023-03-10 21:45:40 +0900

    Show device connected/disconnected icon in Bluetooth device list.

--- btswwin/BluetoothDeviceList.cpp
+++ btswwin/BluetoothDeviceList.cpp
@@ -22,7 +22,7 @@ HRESULT CBluetoothDeviceList::OnInitCtrl()
     setupColumns(columns);
 
     // Setup image list for Bluetooth on/off icon.
-    static const UINT bitmaps[] = {IDB_BITMAP_RADIO_ON, IDB_BITMAP_RADIO_OFF};
+    static const UINT bitmaps[] = {IDB_BITMAP_DEVICE_CONNECTED, IDB_BITMAP_DEVICE_DISCONNECTED};
     setupImageList(bitmaps);
 
     return S_OK;
@@ -64,6 +64,7 @@ HRESULT CBluetoothDeviceList::Update(const BLUETOOTH_DEVICE_INFO& info, UpdateMa
     HR_ASSERT(-1 < nItem, HRESULT_FROM_WIN32(ERROR_NOT_FOUND));
 
     if(mask & UpdateMask::Name) { SetItemText(nItem, int(Column::Name), info.szName); }
+    if(mask & UpdateMask::ConnectIcon) { SetItem(nItem, int(Column::Address), LVIF_IMAGE, nullptr, info.fConnected ? 0 : 1, 0, 0, 0); }
     if(mask & UpdateMask::ConnectText) { SetItemText(nItem, int(Column::Connected), boolToString(info.fConnected)); }
     if(mask & UpdateMask::Authenticated) { SetItemText(nItem, int(Column::Authenticated), boolToString(info.fAuthenticated)); }
     if(mask & UpdateMask::Remembered) { SetItemText(nItem, int(Column::Remembered), boolToString(info.fRemembered)); }

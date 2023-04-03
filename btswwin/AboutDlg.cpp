6419b1f73f2b979001d5d81af3c94e2df5429633
Koji Yano <cozzyy2002@gmail.com>
2023-04-03 22:46:28 +0900

    Move CAboutDlg class to AboutDlg.h/.cpp.

---
+++ btswwin/AboutDlg.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+
+#include "Resource.h"
+#include "AboutDlg.h"
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
--- btswwin/btswwinDlg.cpp
+++ btswwin/btswwinDlg.cpp
@@ -7,47 +7,16 @@
 #include "btswwin.h"
 #include "btswwinDlg.h"
 #include "SettingsDlg.h"
+#include "AboutDlg.h"
 #include "ValueName.h"
 #include "afxdialogex.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
 
-#define DEBUG_LIDSWITCH
+//#define DEBUG_LIDSWITCH
 #endif
 
-
-// CAboutDlg dialog used for App About
-
-class CAboutDlg : public CDialogEx
-{
-public:
-	CAboutDlg();
-
-// Dialog Data
-#ifdef AFX_DESIGN_TIME
-	enum { IDD = IDD_ABOUTBOX };
-#endif
-
-	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
-
-// Implementation
-protected:
-	DECLARE_MESSAGE_MAP()
-};
-
-CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
-{
-}
-
-void CAboutDlg::DoDataExchange(CDataExchange* pDX)
-{
-	CDialogEx::DoDataExchange(pDX);
-}
-
-BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
-END_MESSAGE_MAP()
 
 
 // CbtswwinDlg dialog

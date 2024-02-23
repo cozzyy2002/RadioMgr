c8d22d0f0f3b3b3f88561e3ae92e1084f35c724d
Koji Yano <cozzyy2002@gmail.com>
2024-02-23 16:43:47 +0900

    Fix SafeHandle.h to avoid build error of RadioMgrConsole project.

--- Common/SafeHandle.h
+++ Common/SafeHandle.h
@@ -2,6 +2,18 @@
 
 #include <memory>
 
+// To enable logging of SafeHandle<T> class:
+//	Include header files of log4cxx prior to this file.
+//	Declare `logger` satic member as follows:
+//		/*static*/ log4cxx::LoggerPtr& SafeHandle<T>::logger(log4cxx::Logger::getLogger("Logger name")));
+#if !defined(SAFE_HANDLE_LOG)
+#if defined(LOG4CXX_LOG4CXX_H)
+#define SAFE_HANDLE_LOG 1
+#else
+#define SAFE_HANDLE_LOG 0
+#endif
+#endif
+
 template<typename H>
 using SafeHandleDeleteFunc = void (*)(H);
 
@@ -10,14 +22,20 @@ class SafeHandle {
 public:
 	SafeHandle(H h = (H)0) : m_handle(h)
 	{
+#if SAFE_HANDLE_LOG
 		LOG4CXX_DEBUG(logger, _T("SafeHandle created: HANDLE = ") << h);
+#endif
 	}
 	SafeHandle(SafeHandle&& src) noexcept : m_handle(src.m_handle.release())
 	{
+#if SAFE_HANDLE_LOG
 		LOG4CXX_DEBUG(logger, _T("SafeHandle moved: HANDLE = ") << m_handle.get());
+#endif
 	}
 	SafeHandle& operator=(H h) {
+#if SAFE_HANDLE_LOG
 		LOG4CXX_DEBUG(logger, _T("SafeHandle substitutd: HANDLE = ") << h);
+#endif
 		m_handle.reset(h);
 		return *this;
 	}
@@ -29,12 +47,16 @@ public:
 	SafeHandle& operator=(const SafeHandle&) = delete;
 
 protected:
+#if SAFE_HANDLE_LOG
 	static log4cxx::LoggerPtr& logger;
+#endif
 
 	struct Deleter {
 		using pointer = H;
 		void operator()(H h) {
+#if SAFE_HANDLE_LOG
 			LOG4CXX_DEBUG(logger, _T("SafeHandle deleted: HANDLE = ") << h);
+#endif
 			delFunc(h);
 		}
 	};
